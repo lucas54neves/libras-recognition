d07582f165c79e7e1511dfb01d04bfb33927bb48
Lucas Neves <lucas54neves@gmail.com>
2022-05-09 19:32:00 -0300

    feat: add main file

---
+++ src/main.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from cv2 import (
+    COLOR_BGR2RGB,
+    FILLED,
+    FONT_HERSHEY_PLAIN,
+    VideoCapture,
+    cvtColor,
+    putText,
+    rectangle,
+)
+
+from models.HandDetector import HandDetector
+from utils.check_letter import check_letter
+
+
+def main():
+    st.title("LIBRAS Recognition")
+    run = st.checkbox("Run")
+    FRAME_WINDOW = st.image([])
+    camera = VideoCapture(0)
+    detector = HandDetector()
+
+    letter = ""
+
+    while run:
+        _, frame = camera.read()
+
+        frame = detector.find_hands(frame)
+        list_markings = detector.find_position(frame, 0)
+
+        if len(list_markings) > 0:
+            letter = check_letter(list_markings)
+
+        rectangle(
+            frame,
+            (20, 20),  # Beginning of the rectangle
+            (90, 100),  # End of the rectangle
+            (255, 255, 255),  # Color
+            FILLED,  # cv2.FILLED,
+        )
+        putText(frame, str(letter), (30, 85), FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5)
+
+        frame = cvtColor(frame, COLOR_BGR2RGB)
+
+        FRAME_WINDOW.image(frame)
+
+    st.write("Stopped")
+
+
+if __name__ == "__main__":
+    main()
